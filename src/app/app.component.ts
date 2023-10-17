10a1527046fd3973043a55b9d8de333174ee4f91
EDENYO <EDENYO@D-2-MAOZY-WW-N>
2023-10-17 23:20:33 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,32 @@
 import { Component } from '@angular/core';
+import langJson from '../assets/syntax.json';
+
+interface Token {
+  symbol: string,
+  regex: string
+}
+
+function generateIdentityToken(token: string) : Token{
+  return {
+    symbol: token,
+    regex: token.toLowerCase()
+  };
+}
+
+let TOKENS = [
+  ...(
+    ["SET",
+    "AS" ,"PRESS" , "PLAYER" , 
+    "UP", "DOWN", "LEFT", "RIGHT"]
+    .map(token => generateIdentityToken(token))
+  ),
+
+  { symbol: "COLON", regex: ":" },
+  { symbol: "ARROW", regex: "=>" },
+  { symbol: "EQUAL", regex: "=" },
+  { symbol: "NAME" , regex: "[a-zA-Z]+" },
+  { symbol: "NUMBER" , regex: "[0-9]+" },
+]
 
 @Component({
   selector: 'app-root',
@@ -7,4 +35,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'edenScript';
+  code: string = "";
+
+
+
+  tryMatchToken(word: string): string | null {
+    for(let token of TOKENS){
+      let regex = new RegExp(token.regex,"i");
+      let match = regex.test(word);
+      if(match) {return token.symbol}
+    }
+    return null;
+  }
+
+  parseLine(line: string){
+    let spaceChar = " ";
+    let wordInLine = line.split(spaceChar);
+    for(let word of wordInLine){
+        let res = this.tryMatchToken(word);
+        if(res == null){
+          console.log(`the word ${word} is undentified`);
+        }
+        else{
+          console.log(`match to symbol ${res}`);
+        }
+    };
+  }
+
+  split(code: string){
+    let lines = code.split('\n');
+    lines.forEach(line => {
+      this.parseLine(line);
+    });
+
+  }
+
+  click(){
+    this.split(this.code);
+  }
+
+  constructor(){
+    console.log(langJson.hello);
+    let text = "set";
+    let regex = new RegExp("set","i");
+    let match = regex.test(text);
+    console.log(match);
+  }
 }
