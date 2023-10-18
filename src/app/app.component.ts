f51da9a9493606dc0bd70a94947e161467882254
EDENYO <EDENYO@D-2-MAOZY-WW-N>
2023-10-18 16:27:57 +0300

    fixed building of lexical tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,72 @@ interface Token {
   regex: string
 }
 
-function generateIdentityToken(token: string) : Token{
+interface Rule {
+  symbol: string,
+  tokensSymbolsSequence: string[],
+}
+
+interface SyntaxNode {
+  symbol: string,
+  value: string | SyntaxNode[]
+}
+
+function compareStringArrays(arrL: string[], arrR: string[]): boolean{
+  if(arrL.length !== arrR.length) return false;
+  for(let i=0; i < arrR.length; i++){
+    if (arrL[i] !== arrR[i]) return false;
+  }
+  return true;
+}
+
+interface SyntaxTree {
+  root: SyntaxNode
+}
+
+function generateIdentityTokenLowerCase(token: string) : Token{
   return {
     symbol: token,
     regex: token.toLowerCase()
   };
 }
 
+
+function generateIdentityToken(token: string) : Token{
+  return {
+    symbol: token,
+    regex: token
+  };
+}
+
+let RULES : Rule[] = [
+  {symbol: 'SET_VALUE_COMMAND',
+    tokensSymbolsSequence: ['SET','NAME','EQUAL','NUMBER']
+  },
+  {symbol: 'LINE',
+  tokensSymbolsSequence: ['SET_VALUE_COMMAND']
+},
+  {symbol: 'ROOT',
+    tokensSymbolsSequence: ['LINES']
+  },
+  {symbol: 'LINES',
+    tokensSymbolsSequence: ['LINE','LINES']
+  },
+  {symbol: 'LINES',
+    tokensSymbolsSequence: ['END']
+  }
+]
+
 let TOKENS = [
+  ...(
+    ["ROOT" ,"LINE", "LINES", "END", "SET_VALUE_COMMAND",
+]
+    .map(token => generateIdentityToken(token))
+  ),
   ...(
     ["SET",
     "AS" ,"PRESS" , "PLAYER" , 
     "UP", "DOWN", "LEFT", "RIGHT"]
-    .map(token => generateIdentityToken(token))
+    .map(token => generateIdentityTokenLowerCase(token))
   ),
 
   { symbol: "COLON", regex: ":" },
@@ -48,26 +101,62 @@ export class AppComponent {
     return null;
   }
 
-  parseLine(line: string){
+  tryCollapseUsingRules(nodes: SyntaxNode[], rules: Rule[]): boolean{
+    let isChanged : boolean = false;
+    let treeAsTokensArr : string[] = nodes.map(node => node.symbol);
+    console.log(treeAsTokensArr);
+    for(let i=0; i < treeAsTokensArr.length ; i++){
+      for(let j=i + 1; j < treeAsTokensArr.length ; j++){
+        for(let rule of rules){
+          if(rule.tokensSymbolsSequence.length === (j - i + 1) &&
+              compareStringArrays(rule.tokensSymbolsSequence,treeAsTokensArr.slice(i,j+1))){
+                let children = nodes.splice(i,j+1);
+                nodes.push({symbol: rule.symbol , value: children});
+                isChanged = true;
+          }
+        }
+      }
+    }
+    return isChanged;
+  }
+
+  parseLine(line: string): null | SyntaxNode[] {
     let spaceChar = " ";
     let wordInLine = line.split(spaceChar);
+    let nodes: SyntaxNode[] = [];
     for(let word of wordInLine){
         let res = this.tryMatchToken(word);
         if(res == null){
           console.log(`the word ${word} is undentified`);
+          return null;
         }
         else{
-          console.log(`match to symbol ${res}`);
+          nodes.push({symbol: res , value: word});
         }
     };
+    return nodes;
   }
 
-  split(code: string){
+  split(code: string) : SyntaxTree | null{
     let lines = code.split('\n');
-    lines.forEach(line => {
-      this.parseLine(line);
-    });
+    let linesSyntaxSubtrees : SyntaxNode[] = [];
+    for(let line of lines){
+      let lineTokensNodes = this.parseLine(line);
+      if (lineTokensNodes === null) return null;
+      while (lineTokensNodes.length > 1) {
+        if(!this.tryCollapseUsingRules(lineTokensNodes, RULES)) return null;
+      }
+      linesSyntaxSubtrees.push(lineTokensNodes[0]);
+    }
 
+     console.log(linesSyntaxSubtrees);
+
+
+    while (linesSyntaxSubtrees.length > 1) {
+      if(!this.tryCollapseUsingRules(linesSyntaxSubtrees, RULES)) return null;
+    }
+
+    return {root: linesSyntaxSubtrees[0]}
   }
 
   click(){
